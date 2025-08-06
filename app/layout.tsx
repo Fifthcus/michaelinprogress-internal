7d9f11a6ffb0d557c2b3acd7219b38d6de1cbd20
Michael <171223687+Bazorko@users.noreply.github.com>
2025-08-06 17:57:21 -0400

    - Very simple UI for home page completed.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={``}>
-        {children}
+      <body className="h-screen flex flex-col">
+        <header className="flex flex-col gap-2 self-center text-center py-4">
+          <h1 className="text-xl">Michael in Progress - Internal</h1>
+          <p className="text-xs">Manage blogs, and other data here.</p>
+        </header>
+        <main className="grow content-center items-center self-center">
+          <button className="bg-blue-400 border border-blue-400 text-gray-50 py-2 px-6 rounded-lg cursor-pointer">Log In</button>
+        </main>
       </body>
     </html>
   );
