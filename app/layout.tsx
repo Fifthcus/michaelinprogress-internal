4154cc0676c46cd9347057ac6ab7cb776f622455
Michael <171223687+Bazorko@users.noreply.github.com>
2025-08-06 19:54:33 -0400

    - Moved <header> and <main> and their contents from layout.tsx to page.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,12 @@
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className="h-screen flex flex-col">
-        <header className="flex flex-col gap-2 self-center text-center py-4">
-          <h1 className="text-xl">Michael in Progress - Internal</h1>
-          <p className="text-xs">Manage blogs, and other data here.</p>
-        </header>
-        <main className="grow content-center items-center self-center">
-          <button className="bg-blue-400 border border-blue-400 text-gray-50 py-2 px-6 rounded-lg cursor-pointer">Log In</button>
-        </main>
+        <aside id="mipi-portal">
+        </aside>
+        { children }
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
+import Login from "@/components/Login/Login";
+
 export default function Home() {
   return (
-    <p>hello world</p>
+    <>
+      <header className="flex flex-col gap-2 self-center text-center py-4">
+        <h1 className="text-xl">Michael in Progress - Internal</h1>
+        <p className="text-xs">Manage blogs, and other data here.</p>
+      </header>
+      <main className="grow content-center items-center self-center">
+        <Login/>
+      </main>
+    </>
   );
 }
